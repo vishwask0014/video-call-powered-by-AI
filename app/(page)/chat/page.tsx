5eb0e24f45f3a1706529f6a7c776ac4ac85f093d
vishwas-icodelabs <vishwas.kamal@icodelabs.co>
2026-03-30 10:15:00 +0530

    setup get stream

---
+++ app/(page)/chat/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import page from '../video-call/page'
+
+const Page = () => {
+  return (
+    <div>
+      chat screen
+    </div>
+  )
+}
+
+export default Page
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "stream-chat-react/dist/css/v2/index.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
